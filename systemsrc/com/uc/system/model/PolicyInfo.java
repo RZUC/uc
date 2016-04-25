01c8214426260f5c025f505ca3335ac5aba41d5b
8-l8l-8 <348885497@qq.com>
2016-04-25 17:14:53 +0800

    更新政策消息参数

--- systemsrc/com/uc/system/model/PolicyInfo.java
+++ systemsrc/com/uc/system/model/PolicyInfo.java
@@ -10,18 +10,20 @@
 */
 package com.uc.system.model;
 
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.mongodb.core.mapping.TextScore;
+
 /**
- * @Description: 政策信息
- * 倒叙排列
+ * @Description: 政策信息 倒叙排列
  * @ClassName: PolicyInfo
  * @author 落花流水
  * @date 2016年4月7日 上午11:47:11
  */
 public class PolicyInfo
 {
-    private String title;// 政策标题
+    private @TextScore String title;// 政策标题
     
     private String sourceUrl;// 发布来源ID
     
@@ -33,60 +35,11 @@ public class PolicyInfo
     
     private String location;// 区域
     
-    private int topState;//置顶字段
+    private int topState;// 置顶字段
     
-    private int order;//排序字段
-    private int topStateEndTime;//信息有效期，有效期到后自动消除置顶
+    private int order;// 排序字段
     
-    /**
-     * @Title: PolicyInfo
-     * @Description: TODO(这里用一句话描述这个方法的作用)
-     * @param @param title
-     * @param @param sourceUrl
-     * @param @param department
-     * @param @param policyType
-     * @param @param industry
-     * @param @param location
-     * @param @param province
-     * @param @param city
-     * @param @param downtown
-     * @param @param content
-     * @param @param releaseTime
-     * @param @param createTime
-     * @param @param resourceList 设定文件
-     * @return
-     */
-    public PolicyInfo(String title, String sourceUrl, String department, String policyType, String industry,
-        String location, String province, String city, String downtown, String content, String releaseTime,
-        String createTime, List<Resource> resourceList)
-    {
-        super();
-        this.title = title;
-        this.sourceUrl = sourceUrl;
-        this.department = department;
-        this.policyType = policyType;
-        this.industry = industry;
-        this.location = location;
-        this.province = province;
-        this.city = city;
-        this.downtown = downtown;
-        this.content = content;
-        this.releaseTime = releaseTime;
-        this.createTime = createTime;
-        this.resourceList = resourceList;
-    }
-    
-    /**
-     * @Title: PolicyInfo
-     * @Description: TODO(这里用一句话描述这个方法的作用)
-     * @param 设定文件
-     * @return
-     */
-    public PolicyInfo()
-    {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    private int topStateEndTime;// 信息有效期，有效期到后自动消除置顶
     
     private String province;// 省
     
@@ -98,10 +51,70 @@ public class PolicyInfo
     
     private String releaseTime;// 发布时间
     
-    private String createTime;// 添加时间
-    
     private List<Resource> resourceList;// 资源文件
     
+    private Date createTime;
+    
+    private Date lastUpdateTime;
+    
+    /**
+     * @Title: PolicyInfo
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @param 设定文件
+     * @return
+     */
+    public PolicyInfo()
+    {
+        super();
+    }
+    
+    /** 
+     * @Title: PolicyInfo
+     * @Description: TODO(这里用一句话描述这个方法的作用) 
+     * @param @param title
+     * @param @param sourceUrl
+     * @param @param department
+     * @param @param policyType
+     * @param @param industry
+     * @param @param location
+     * @param @param topState
+     * @param @param order
+     * @param @param topStateEndTime
+     * @param @param province
+     * @param @param city
+     * @param @param downtown
+     * @param @param content
+     * @param @param releaseTime
+     * @param @param resourceList
+     * @param @param createTime
+     * @param @param lastUpdateTime 设定文件 
+     * @return  
+     */ 
+    public PolicyInfo(String title, String sourceUrl, String department, String policyType, String industry,
+        String location, int topState, int order, int topStateEndTime, String province, String city, String downtown,
+        String content, String releaseTime, List<Resource> resourceList, Date createTime, Date lastUpdateTime)
+    {
+        super();
+        this.title = title;
+        this.sourceUrl = sourceUrl;
+        this.department = department;
+        this.policyType = policyType;
+        this.industry = industry;
+        this.location = location;
+        this.topState = topState;
+        this.order = order;
+        this.topStateEndTime = topStateEndTime;
+        this.province = province;
+        this.city = city;
+        this.downtown = downtown;
+        this.content = content;
+        this.releaseTime = releaseTime;
+        this.resourceList = resourceList;
+        this.createTime = createTime;
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+
     /**
      * @return the title
      */
@@ -188,14 +201,6 @@ public class PolicyInfo
     public String getReleaseTime()
     {
         return releaseTime;
-    }
-    
-    /**
-     * @return the createTime
-     */
-    public String getCreateTime()
-    {
-        return createTime;
     }
     
     /**
@@ -295,19 +300,91 @@ public class PolicyInfo
     }
     
     /**
-     * @param createTime the createTime to set
-     */
-    public void setCreateTime(String createTime)
-    {
-        this.createTime = createTime;
-    }
-    
-    /**
      * @param resourceList the resourceList to set
      */
     public void setResourceList(List<Resource> resourceList)
     {
         this.resourceList = resourceList;
+    }
+    
+    /**
+     * @return the topState
+     */
+    public int getTopState()
+    {
+        return topState;
+    }
+    
+    /**
+     * @return the order
+     */
+    public int getOrder()
+    {
+        return order;
+    }
+    
+    /**
+     * @return the topStateEndTime
+     */
+    public int getTopStateEndTime()
+    {
+        return topStateEndTime;
+    }
+    
+    /**
+     * @return the lastUpdateTime
+     */
+    public Date getLastUpdateTime()
+    {
+        return lastUpdateTime;
+    }
+    
+    /**
+     * @param topState the topState to set
+     */
+    public void setTopState(int topState)
+    {
+        this.topState = topState;
+    }
+    
+    /**
+     * @param order the order to set
+     */
+    public void setOrder(int order)
+    {
+        this.order = order;
+    }
+    
+    /**
+     * @param topStateEndTime the topStateEndTime to set
+     */
+    public void setTopStateEndTime(int topStateEndTime)
+    {
+        this.topStateEndTime = topStateEndTime;
+    }
+    
+    /**
+     * @param createTime the createTime to set
+     */
+    public void setCreateTime(Date createTime)
+    {
+        this.createTime = createTime;
+    }
+    
+    /**
+     * @param lastUpdateTime the lastUpdateTime to set
+     */
+    public void setLastUpdateTime(Date lastUpdateTime)
+    {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+    
+    /**
+     * @return the createTime
+     */
+    public Date getCreateTime()
+    {
+        return createTime;
     }
     
 }
