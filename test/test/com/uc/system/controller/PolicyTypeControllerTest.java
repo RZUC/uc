8857f545ec9c007e19384fe246883030b2c85fe9
348885497@qq.com <1q2w3e4r>
2016-05-15 15:16:27 +0800

    政策类型测试

--- test/test/com/uc/system/controller/PolicyTypeControllerTest.java
+++ test/test/com/uc/system/controller/PolicyTypeControllerTest.java
@@ -3,20 +3,30 @@
  */
 package test.com.uc.system.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.uc.system.servlet.PolicyTypeController;
 
 import test.object.ObjectWebTest;
 
 /**
- * @Description: TODO(政策类型Servlet)
+ * @Description: TODO(这里用一句话描述这个类的作用)
  * @ClassName: PolicInfoControllerTest
  * @author 落花流水
  * @date 2016年4月30日 上午11:53:34
  */
-public class PolicInfoControllerTest extends ObjectWebTest {
+public class PolicyTypeControllerTest extends ObjectWebTest {
+
+	@Before
+	public void setUp() {
+		PolicyTypeController colltroller = new PolicyTypeController();
+		mockMvc = MockMvcBuilders.standaloneSetup(colltroller).build();
+	}
 
 	@Test
 	public void mocMveTest() {
@@ -25,7 +35,7 @@ public class PolicInfoControllerTest extends ObjectWebTest {
 			result = mockMvc
 					.perform(
 							MockMvcRequestBuilders
-									.get("/login.do?username=renhao&password=123456"))
+									.get("/policyType/showAllPolicyType.do"))
 					// .andExpect(MockMvcResultMatchers.view().name("user/view"))
 					// .andExpect(MockMvcResultMatchers.model().attributeExists("user"))
 					.andDo(MockMvcResultHandlers.print()).andReturn();
---
+++ test/test/com/uc/system/service/PolicyTypeServiceTest.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package test.com.uc.system.service;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import test.object.ObjectTest;
+
+import com.uc.system.exception.ZhiWeiException;
+import com.uc.system.model.Page;
+import com.uc.system.model.PolicyType;
+import com.uc.system.service.PolicyTypeService;
+
+/**
+ * @author Administrator 政策类型修改
+ */
+public class PolicyTypeServiceTest extends ObjectTest {
+
+	@Resource
+	PolicyTypeService service;
+
+	@Test
+	public void add() throws ZhiWeiException {
+
+		PolicyType type = new PolicyType();
+		type.setId("1");
+		type.setName("政策服务");
+		type.setOrder(1);
+		service.addPolicyType(type);
+	}
+
+	@Test
+	public void del() throws ZhiWeiException {
+
+		System.out.println(service.delPolicyType("1"));
+	}
+
+	@Test
+	public void modif() throws ZhiWeiException {
+
+		PolicyType type = new PolicyType();
+		type.setId("1");
+		type.setName("政策服务2");
+		type.setOrder(2);
+		service.addPolicyType(type);
+	}
+
+	@Test
+	public void findAll() throws ZhiWeiException {
+		Page page = new Page();
+		page.setPageSize(10);
+		page.setPageNum(1);
+		System.out.println(service.findByPage(page).get(0).toString());
+	}
+}
