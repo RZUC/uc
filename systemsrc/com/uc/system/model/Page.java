883054843fe8cb67963a7a9524b6f5b9c214e0d9
348885497@qq.com <1q2w3e4r>
2016-05-27 23:04:19 +0800

    删除两个参数

--- systemsrc/com/uc/system/model/Page.java
+++ systemsrc/com/uc/system/model/Page.java
@@ -9,8 +9,6 @@ package com.uc.system.model;
 public class Page {
 	private int pageSize;// 每页条数
 	private int pageNum;// 当前页面
-	private int totalPage;// 总页数
-	private int totalSize;// 总条数
 
 	public int getPageSize() {
 		return pageSize;
@@ -26,21 +24,5 @@ public class Page {
 
 	public void setPageNum(int pageNum) {
 		this.pageNum = pageNum;
-	}
-
-	public int getTotalPage() {
-		return totalPage;
-	}
-
-	public void setTotalPage(int totalPage) {
-		this.totalPage = totalPage;
-	}
-
-	public int getTotalSize() {
-		return totalSize;
-	}
-
-	public void setTotalSize(int totalSize) {
-		this.totalSize = totalSize;
 	}
 }
