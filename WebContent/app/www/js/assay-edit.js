fbdd3f5c42ce5c262a050da3aec97f3ced3f87a0
lihuazhidao <765794900@qq.com>
2016-06-02 23:33:55 +0800

    assay-edit.html

--- WebContent/app/www/js/assay-edit.js
+++ WebContent/app/www/js/assay-edit.js
@@ -5,44 +5,14 @@
 
 /* exported initSample */
 
-if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
-	CKEDITOR.tools.enableHtml5Elements( document );
 
 
-CKEDITOR.replace( 'editor', {
-					extraPlugins: 'bbcode',
-					// Remove unused plugins.
-					// removePlugins: 'bidi,dialogadvtab,div,filebrowser,flash,format,forms,horizontalrule,iframe,justify,liststyle,pagebreak,showborders,stylescombo,table,tabletools,templates',
-					// Width and height are not supported in the BBCode format, so object resizing is disabled.
-					disableObjectResizing: true,
-					// Define font sizes in percent values.
-					fontSize_sizes: "30/30%;50/50%;100/100%;120/120%;150/150%;200/200%;300/300%",
-					toolbar: [
-						[ 'Source','-', 'Undo', 'Redo' ],
-						[ 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat' ],
-						[ 'Link', 'Unlink', 'Image', 'Smiley', 'SpecialChar' ],
-						[ 'Bold', 'Italic', 'Underline' ],
-						[ 'FontSize' ],
-						[ 'TextColor' ],
-						[ 'NumberedList', 'BulletedList', '-', 'Blockquote' ],
-						[ 'Maximize' ],
-						['Save']
 
-					],
-					// Strip CKEditor smileys to those commonly used in BBCode.
-					smiley_images: [
-						'regular_smile.png', 'sad_smile.png', 'wink_smile.png', 'teeth_smile.png', 'tongue_smile.png',
-						'embarrassed_smile.png', 'omg_smile.png', 'whatchutalkingabout_smile.png', 'angel_smile.png',
-						'shades_smile.png', 'cry_smile.png', 'kiss.png'
-					],
-					smiley_descriptions: [
-						'smiley', 'sad', 'wink', 'laugh', 'cheeky', 'blush', 'surprise',
-						'indecision', 'angel', 'cool', 'crying', 'kiss'
-					]
-				});
+$(function(){
+      CKEDITOR.replace( 'editor',{
+      		removePlugins: 'bidi,dialogadvtab,div,save,print,filebrowser,flash,format,forms,horizontalrule,iframe,pagebreak,showborders,stylescombo,table,tabletools,templates',
+      } );
 
-
-console.log(CKEDITOR)
-console.log(CKEDITOR.dom)
-console.log(CKEDITOR.dom.text())
+CKEDITOR.config.height=450;
+})
 
