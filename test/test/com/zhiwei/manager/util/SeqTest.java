34ae5739037f5e727ef45163f3042362c890f829
悟 <348885497@qq.com>
2016-06-17 23:12:15 +0800

    squencec测试测试

---
+++ test/test/com/zhiwei/manager/util/SeqTest.java
@@ -0,0 +1,25 @@
+/** 
+ * Copyright (C), NingBo ZhiWeiReach info. Co., Ltd. 
+ * FileName: Test.java 
+ * 类的详细说明 
+ * 
+ * @author 东临碣石
+ * @Date 2016年1月15日
+ * @version 1.00 
+ */
+package test.com.zhiwei.manager.util;
+
+import com.uc.system.util.Sequence;
+
+/**
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @ClassName: Test
+ * @author 落花流水
+ * @date 2016年1月15日 上午9:50:43
+ */
+public class SeqTest {
+	public static void main(String[] args) {
+		System.out.println(Sequence.getNextId("policyInfo"));
+	}
+
+}
