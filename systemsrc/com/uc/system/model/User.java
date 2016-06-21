f0512bb70db30fa252b5bb1d938881a9ab28aed6
悟 <348885497@qq.com>
2016-06-21 22:54:54 +0800

    提供完成的用户类

--- systemsrc/com/uc/system/model/User.java
+++ systemsrc/com/uc/system/model/User.java
@@ -32,57 +32,32 @@ public class User {
 	private String password;
 	private String createTime;// 创建时间
 	private String whence;// 来源 导入，或者注册
-	private String userTypeId;// 企业 服务机构
 	private int province;// 省 （ID）
 	private int city;// 市 （ID）
 	private int downtown;// 区
 
-	int industryLevelOneId;
-	int industryLevelTwoId;
+	private String userTypeId;// 企业 服务机构
+	int industryLevelOneId;// 技术领域
+	int industryLevelTwoId;// 技术领域
+	private String Linkman;// 联系人
+	private String mobilephone;// 联系电话
 
 	/* 企业信息 */
 	private String enterpriseName;// 企业名称
 	private String enterpriseAddress;// 企业地址
-	private String Linkman;// 联系人
-	private String telephone;// 联系电话
 
 	/* 服务机构 */
-	// TODO:服务机构类别包括：政策咨询、管理咨询、知识产权、认证评估、科技服务、人力资源服务、会计/审计/税务服务、法律服务、金融服务、广告/媒体/会展、研发设计、IT服务
-	
-	String organizationName;// 机构名称
-	int organizationType;// 机构类别
-	int organizProvince;
-	int organizCity;
-	int organizDowntown;
-	String url;//网址
-	String summary;//简介
-	String serviceContent;//服务内容
-	String serviceContentDescription;//服务内容描述
-	String serviceSubjectTerm;//服务主题词
-	String regiseAddress;
- 
-	
-	public User() {
-		super();
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", password=" + password
-				+ ", createTime=" + createTime + ", whence=" + whence + "]";
-	}
-
-	public String getUserTypeId() {
-		return userTypeId;
-	}
-
-	public void setUserTypeId(String userTypeId) {
-		this.userTypeId = userTypeId;
-	}
-
-	public int getProvince() {
-		return province;
-	}
+	private String organizationName;// 机构名称
+	private int organizationType;// 机构类别
+	private String organizLocation; // 服务区域
+	private String url;// 网址
+	private String summary;// 简介
+	private String serviceContent;// 可提供服务
+	private String serviceContentDescription;// 服务内容描述
+	private String regiseAddress;
+	private String telephone;// 固定电话
+	private String department;// 部门
+	private String job;// 职务
 
 	public User(String id, String name, String password, String createTime,
 			String whence, String userTypeId, int province, int city,
@@ -97,6 +72,55 @@ public class User {
 		this.province = province;
 		this.city = city;
 		this.downtown = downtown;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getWhence() {
+		return whence;
+	}
+
+	public void setWhence(String whence) {
+		this.whence = whence;
+	}
+
+	public int getProvince() {
+		return province;
 	}
 
 	public void setProvince(int province) {
@@ -119,78 +143,147 @@ public class User {
 		this.downtown = downtown;
 	}
 
-	/**
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
+	public String getUserTypeId() {
+		return userTypeId;
 	}
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
+	public void setUserTypeId(String userTypeId) {
+		this.userTypeId = userTypeId;
 	}
 
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
+	public int getIndustryLevelOneId() {
+		return industryLevelOneId;
 	}
 
-	/**
-	 * @return the createTime
-	 */
-	public String getCreateTime() {
-		return createTime;
+	public void setIndustryLevelOneId(int industryLevelOneId) {
+		this.industryLevelOneId = industryLevelOneId;
 	}
 
-	/**
-	 * @return the whence
-	 */
-	public String getWhence() {
-		return whence;
+	public int getIndustryLevelTwoId() {
+		return industryLevelTwoId;
 	}
 
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(String id) {
-		this.id = id;
+	public void setIndustryLevelTwoId(int industryLevelTwoId) {
+		this.industryLevelTwoId = industryLevelTwoId;
 	}
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
+	public String getLinkman() {
+		return Linkman;
 	}
 
-	/**
-	 * @param password
-	 *            the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
+	public void setLinkman(String linkman) {
+		Linkman = linkman;
 	}
 
-	/**
-	 * @param createTime
-	 *            the createTime to set
-	 */
-	public void setCreateTime(String createTime) {
-		this.createTime = createTime;
+	public String getMobilephone() {
+		return mobilephone;
 	}
 
-	/**
-	 * @param whence
-	 *            the whence to set
-	 */
-	public void setWhence(String whence) {
-		this.whence = whence;
+	public void setMobilephone(String mobilephone) {
+		this.mobilephone = mobilephone;
+	}
+
+	public String getEnterpriseName() {
+		return enterpriseName;
+	}
+
+	public void setEnterpriseName(String enterpriseName) {
+		this.enterpriseName = enterpriseName;
+	}
+
+	public String getEnterpriseAddress() {
+		return enterpriseAddress;
+	}
+
+	public void setEnterpriseAddress(String enterpriseAddress) {
+		this.enterpriseAddress = enterpriseAddress;
+	}
+
+	public String getOrganizationName() {
+		return organizationName;
+	}
+
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+
+	public int getOrganizationType() {
+		return organizationType;
+	}
+
+	public void setOrganizationType(int organizationType) {
+		this.organizationType = organizationType;
+	}
+
+	public String getOrganizLocation() {
+		return organizLocation;
+	}
+
+	public void setOrganizLocation(String organizLocation) {
+		this.organizLocation = organizLocation;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getServiceContent() {
+		return serviceContent;
+	}
+
+	public void setServiceContent(String serviceContent) {
+		this.serviceContent = serviceContent;
+	}
+
+	public String getServiceContentDescription() {
+		return serviceContentDescription;
+	}
+
+	public void setServiceContentDescription(String serviceContentDescription) {
+		this.serviceContentDescription = serviceContentDescription;
+	}
+
+	public String getRegiseAddress() {
+		return regiseAddress;
+	}
+
+	public void setRegiseAddress(String regiseAddress) {
+		this.regiseAddress = regiseAddress;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
 	}
 }
