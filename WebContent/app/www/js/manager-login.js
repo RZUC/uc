ad48893857a229d1984474eab6ca2c1a6c8909ea
lihuazhidao <765794900@qq.com>
2016-06-27 23:02:42 +0800

    后台登录

--- WebContent/app/www/js/manager-login.js
+++ WebContent/app/www/js/manager-login.js
@@ -14,3 +14,57 @@
           this.errors.password=""
         }
 */
+function saveLogin(data){
+  date = new Date();
+  date.setTime( date.getTime() + ( 0.5 * 60 * 60 * 1000 ));
+  $.cookie("admin",JSON.stringify(data),{ expires: date });
+};
+
+
+
+function login(evt){
+  evt.preventDefault();
+  evt.stopPropagation();
+
+  var user=JSON.parse(JSON.stringify(this.user));
+
+
+    $.ajax({
+      url:"../../admin/login.do",
+      data:user,
+      type:"POST",
+      dataType:"json",
+      success:function(data){
+          if(!data.state){
+            alert(data.message)
+          }else{
+            saveLogin(data.data);
+          }
+      },
+      error(err){
+          console.log(err)
+      }
+
+
+
+        })
+
+
+
+}
+
+
+
+new Vue({
+  el:"#manager-login",
+  data:{
+    user:{
+      username:"",
+      password:""
+    }
+  },
+  methods:{
+    login:login
+  }
+})
+
