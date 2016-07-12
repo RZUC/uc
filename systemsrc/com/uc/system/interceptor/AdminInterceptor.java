82070ef5287bcf30007dc36cc65446222a412f66
悟 <348885497@qq.com>
2016-07-12 10:24:39 +0800

    后台用户登录管理器

--- systemsrc/com/uc/system/interceptor/AdminInterceptor.java
+++ systemsrc/com/uc/system/interceptor/AdminInterceptor.java
@@ -25,39 +25,53 @@ import com.uc.system.model.AdminUser;
  * @author 落花流水
  * @date 2016年1月27日 下午4:44:54
  */
-public class AdminInterceptor extends HandlerInterceptorAdapter {
-	private String LOGINURL = "/admin.do";
-	private String UNLOGINURL = "/adminLogin.do";
-
-	@Override
-	public void afterCompletion(HttpServletRequest arg0,
-			HttpServletResponse arg1, Object arg2, Exception arg3)
-			throws Exception {
-		System.out.println("this is afterCompletion");
-	}
-
-	@Override
-	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
-			Object arg2, ModelAndView arg3) throws Exception {
-		System.out.println("this is postHandle");
-	}
-
-	@Override
-	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1,
-			Object arg2) throws Exception {
-		System.out.println(arg0.getRequestURL());
-		boolean result = true;
-
-//		HttpSession session = arg0.getSession();
-//		try {
-//			AdminUser user = (AdminUser) session.getAttribute("admin");
-//			System.out.println(user.toString());
-//		} catch (NullPointerException e) {
-//			System.out.println("请先登录");
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		return result;
-	}
-
+public class AdminInterceptor extends HandlerInterceptorAdapter
+{
+    private String LOGINURL = "/admin.do";
+    
+    private String UNLOGINURL = "/adminLogin.do";
+    
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+        throws Exception
+    {
+        System.out.println("this is afterCompletion");
+    }
+    
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+        throws Exception
+    {
+        System.out.println("this is postHandle");
+    }
+    
+    @Override
+    public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2)
+        throws Exception
+    {
+        String baseContenxtParth = arg0.getSession().getServletContext().getContextPath();
+        System.out.println("--"+arg0.getSession().getServletContext().getRequestDispatcher("/adc.do").toString());
+        boolean result = true;
+        
+        HttpSession session = arg0.getSession();
+        AdminUser user = null;
+        try
+        {
+            user = (AdminUser)session.getAttribute("admin");
+            
+            System.out.println(arg0.getSession().getServletContext().getContextPath());
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("请先登录");
+            
+//            e.printStackTrace();
+        }
+//        if (null == user)
+//        {
+//            arg1.sendRedirect(baseContenxtParth + "/app/www/manager-login.html");
+//        }
+        return result;
+    }
+    
 }
